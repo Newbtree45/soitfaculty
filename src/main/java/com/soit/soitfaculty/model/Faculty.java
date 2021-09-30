ead85687ab7a62fc1af7d2096b0565f479d64bbd
Newbtree45 <newberts@mail.uc.edu>
2021-09-30 14:21:05 -0400

    Initial commit

---
+++ src/main/java/com/soit/soitfaculty/model/Faculty.java
@@ -0,0 +1,69 @@
+package com.soit.soitfaculty.model;
+
+public class Faculty {
+	
+	private int id;
+	private String firstName;
+	private String lastName;	
+	private String email;
+	
+	/***
+	 * A blank Faculty constructor
+	 */
+	public Faculty() {
+		
+	}
+
+	/***
+	 * A Faculty constructor that takes parameters.
+	 * @param id Id of the faculty
+	 * @param firstName First name of the faculty
+	 * @param lastName Last name of the faculty
+	 * @param email Email of the faculty
+	 */
+	public Faculty(int id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Faculty [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+
+}
